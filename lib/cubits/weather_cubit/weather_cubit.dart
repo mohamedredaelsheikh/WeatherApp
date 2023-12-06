e2c8616d097b9e0fd4a5c2283d03a7211640eaec
Mohammed_Elsheikh <moh.reda198@gmail.com>
2023-12-06 21:04:30 +0200

    create weather cubit

--- lib/cubits/weather_cubit/weather_cubit.dart
+++ lib/cubits/weather_cubit/weather_cubit.dart
@@ -1,1 +1,20 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project/cubits/weather_cubit/weather_states.dart';
+import 'package:project/models/weather_model.dart';
+import 'package:project/services/weather_service.dart';
 
+class WeatherCubit extends Cubit<WeatherState> {
+  WeatherCubit({required this.weatherservice}) : super(WeatherInitialState());
+  WeatherModel? weathermodel;
+  WeatherService weatherservice;
+  String? cityname;
+  void getWeather({required String cityName}) async {
+    emit(WeatherLoadingState());
+    try {
+      weathermodel = await weatherservice.getWeather(cityName: cityName);
+      emit(WeatherSuccessState());
+    } on Exception catch (e) {
+      emit(WeatherFailureState());
+    }
+  }
+}
